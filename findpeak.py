f2e11e031c438fd8dac4cd8a15243c00c1b97959
Rex Tsai <chihchun@kalug.linux.org.tw>
2013-10-05 01:29:44 +0800

    列出所有山嶽
    Signed-off-by: Rex Tsai <chihchun@kalug.linux.org.tw>

--- findpeak.py
+++ findpeak.py
@@ -7,55 +7,7 @@ import sys
 from pprint import pprint
 from subprocess import call
 
-delta = 0.002
-readonly = True
-debug = True
-
-osm = OsmApi.OsmApi(passwordfile="passwd", appid = 'RexBot', debug=debug, changesetauto=False)
-
-stones = [
-"一等三角點基線點",
-"一等三角點本點",
-"一等三角點補點",
-"二等三角點",
-"三等三角點",
-"四等三角點",
-"森林三角點",
-#"土地調查局測量原點",
-#"土地調查局三等三角點",
-#"內政部一等三角點補點",
-#"內政部二等三角點補點",
-#"內政部三等三角點補點",
-#"聯勤二等三角點補點",
-#"聯勤三等三角點補點",
-#"陸軍三等三角點補點",
-#"千岳俱樂部仿製二等三角點",
-#"千岳俱樂部仿製三等三角點",
-#"仿製三等三角點",
-#"土地調查局圖根點",
-#"圖根補點",
-#"總督府圖根補點",
-#"殖產局三角補點",
-#"殖產局圖根三角補點",
-#"殖產局圖根補點",
-#"殖產局補點",
-#"省政府圖根補點",
-#"總督府三角點",
-#"總督府點",
-#"總督府柱",
-#"內務局三角點" 
-#"內務局補助三角點",
-#"一等水準點",
-#"北市三角點",
-#"北市都市計畫點",
-#"北市地測點",
-#"北市地測精幹點",
-#"北市",
-#"北縣三角點",
-#"北縣都市計畫點",
-#"基隆",
-]
-
+stones = sys.argv[1:]
 
 delta = 0.002
 readonly = True
@@ -98,19 +50,16 @@ def main():
             # ignore data without lat, lon
             if(len(row[7]) == 0 or len(row[8]) == 0):
                 continue
-            # ignore if the stone is missing.
-            if(len(row[12]) > 0):
-                continue
+
             # refine the format
-     
             row[5] = row[5].replace("◎本點","")
             row[5] = row[5].replace("●","")
             lat = row[7] = float(row[7]) # lat
             lon = row[8] = float(row[8]) # lon
-            ele = row[2] = float(row[2].replace(",",""))
+            ele = row[2] = row[2].replace(",","")
     
-            if(row[3] not in stones and (ispeak(row[0] or ispeak(row[1])))):
-                print(row[3])
+            if((len(stones) == 0 or row[3] in stones) and (ispeak(row[0] or ispeak(row[1])))):
+                print(row[0])
 
 if __name__ == '__main__':
     main()
